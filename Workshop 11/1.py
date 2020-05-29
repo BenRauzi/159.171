6f308cfc2b459f250cc6e90c121cc8bab864c943
Ben Rauzi <ben.rauzi@outlook.com>
2020-05-29 17:00:30 +1200

    All Workshop Answers under 1.py (All were compatible with one another)

---
+++ Workshop 11/1.py
@@ -0,0 +1,16 @@
+class Food:
+    def __init__(self, name, taste):
+        self.name = name
+        self.taste = taste
+    def __str__(self):
+        return f"{self.taste} {self.name}"
+    def orderFood(self, number):
+        print(f"{number} times {self.taste} {self.name}")
+
+class NZFood(Food):
+    origin = "New Zealand"
+    def changeOrigin(self, new_origin):
+        NZFood.origin = new_origin
+
+def createFood(food_list):
+    return [Food(i[0], i[1]) for i in food_list]

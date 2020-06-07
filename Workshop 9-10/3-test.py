a4c399db007ed7e5d8269bd45768f14056565209
Ben Rauzi <ben.rauzi@outlook.com>
2020-06-07 16:47:37 +1200

    Update 3-test.py

--- Workshop 9-10/3-test.py
+++ Workshop 9-10/3-test.py
@@ -2,11 +2,5 @@ import three
 
 #help("three")
 
-#three.filep = "food.csv"
-#three.menu_list()
-#three.show_menu()
-
-list = [1]
-
-if float(1) in list:
-    print(1)
+three.filep = "food.csv"
+three.show_menu()
